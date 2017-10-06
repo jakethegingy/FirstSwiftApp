21b7c64c4de6bfdd64524c1833ec8bf46726ab8d
jakethegingy <31513857+jakethegingy@users.noreply.github.com>
2017-10-06 08:29:16 -0600

    Creating my first swift project.

---
+++ FirstSwiftApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstSwiftApp
+//
+//  Created by Hammond, Jacob on 10/6/17.
+//  Copyright © 2017 Hammond, Jacob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
